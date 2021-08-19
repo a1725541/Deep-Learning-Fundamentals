623132dad2036b57884affe7b25297f79bc187ea
a1725541 <a1725541@student.adelaide.edu.au>
2021-08-20 01:53:29 +0930

    Added comments

--- Assignment 1/diabetes.py
+++ Assignment 1/diabetes.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 
+# Sign function
 def sign(n):
     if n > 0:
         return 1
@@ -10,9 +11,10 @@ def sign(n):
         return -1
 
 
+# Perceptron train
 def perceptron(x, y, eta):
     w = np.zeros((1, np.size(x, 1)))
-    # print(validation(x, w, y))
+    # Repeat 1000 times
     for T in range(1000):
         yx = np.zeros((1, np.size(x, 1)))
         for t in range(len(x)):
@@ -25,10 +27,11 @@ def perceptron(x, y, eta):
     return w
 
 
+# Perceptron which stops after passing error threshold
 def perceptronAlt(x, y, eta):
     w = np.zeros((1, np.size(x, 1)))
     T = 0
-    while test(w, x, y)[0] > 0.31 and T < 10000:
+    while test(w, x, y)[0] > 0.30 and T < 10000:
         T += 1
         yx = np.zeros((1, np.size(x, 1)))
         for t in range(len(x)):
@@ -41,6 +44,7 @@ def perceptronAlt(x, y, eta):
     return w, T
 
 
+# Returns zero-one loss
 def test(w, x, y):
     error = 0
     count = 0
@@ -54,15 +58,17 @@ def test(w, x, y):
     return error / len(x), count
 
 
+# Read data into pandas
 diabetesData = pd.read_csv("diabetes.csv")
 # Get y values
 diabetesOutcome = diabetesData["Outcome"]
 diabetesOutcome[diabetesOutcome == 0] = -1
 # Get x values
 diabetesData = diabetesData.drop("Outcome", axis=1)
+# Convert to np array
 diabetesData = diabetesData.to_numpy()
 diabetesOutcome = diabetesOutcome.to_numpy()
-
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(diabetesData, diabetesOutcome, random_state=1)
 
 etas = [1, 0.01, 0.001, 0.0001]
--- Assignment 1/diabetes_knn.py
+++ Assignment 1/diabetes_knn.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set()
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+sns.set()
 
 
 diabetesData = pd.read_csv("diabetes.csv")
@@ -13,18 +13,21 @@ diabetesOutcome = diabetesData["Outcome"]
 diabetesOutcome[diabetesOutcome == 0] = -1
 # Get x values
 diabetesData = diabetesData.drop("Outcome", axis=1)
+# Convert to np array
 diabetesData = diabetesData.to_numpy()
 diabetesOutcome = diabetesOutcome.to_numpy()
-
+# Split data
 X_train, X_test, y_train, y_test = train_test_split(diabetesData, diabetesOutcome, random_state=1)
 neighbours = np.arange(1, 100)
 test_accuracy = np.empty(len(neighbours))
 
+# Interate for k = 1 to k = 100
 for i, k in enumerate(neighbours):
     knn = KNeighborsClassifier(n_neighbors=k)
     knn.fit(X_train, y_train)
     test_accuracy[i] = 1 - knn.score(X_test, y_test)
 
+# Matplotlib plot
 plt.plot(neighbours, test_accuracy)
 plt.title("$k$ nearest neighbours with diabetes data.\nMinimum error $=$" 
     + str(round(min(test_accuracy), 3)))
